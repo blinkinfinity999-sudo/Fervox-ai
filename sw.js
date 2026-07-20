3bb28c12fce8f7c2dea15aba3fd4e4abae1e58d7
blinkinfinity999-sudo <blinkinfinity999@gmail.com>
2026-07-20 15:16:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js', { scope: './' })
-      .then(reg => console.log('Fervox SW Registered!', reg.scope))
-      .catch(err => console.log('SW Registry Failed:', err));
-  });
-}
+<link rel="manifest" href="manifest.json">
+
+<script>
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js');
+  }
+</script>
